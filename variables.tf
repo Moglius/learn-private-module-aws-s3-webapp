e3d26517ea7c425457ad894fa951c3efea4f88c2
Matias <matiasmoglia@gmail.com>
2023-12-22 12:55:13 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,21 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-variable "region" {}
-variable "prefix" {}
-variable "name" {}
+variable "region" {
+  type = string
+  description = "AWS region"
+  default = "us-east-2"
+}
+
+variable "prefix" {
+  type = string
+  description = "S3 prefix name"
+  default = "noname-"
+}
+
+variable "name" {
+  type = string
+  description = "S3 name"
+  default = "s3-webapp-21321"
+}
+
